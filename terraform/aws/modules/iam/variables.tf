558789341278765d5149898d4072441bd3c73d19
Alex Skoro <skoro23@gmail.com>
2026-04-08 14:44:41 +1000

    vars

--- terraform/aws/modules/iam/variables.tf
+++ terraform/aws/modules/iam/variables.tf
@@ -4,14 +4,14 @@ variable "tags" {
   default     = {}
 }
 
-variable "secret_name" {
-  type        = string
-  description = "Name of the secret in AWS Secrets Manager used for Cloud Connector registration to Zscaler"
-  default     = null
-}
-
 variable "name_prefix" {
   type        = string
   description = "A prefix to associate to all the network module resources"
+  default     = null
+}
+
+variable "secret_name" {
+  type        = string
+  description = "Name of the secret in AWS Secrets Manager used for Cloud Connector registration to Zscaler"
   default     = null
 }
